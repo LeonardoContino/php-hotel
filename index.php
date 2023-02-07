a9043fce6c29a09efc25229418d7a5c9aae4fb2d
LeonardoContino <leoconti2000@hotmail.it>
2023-02-07 15:24:56 +0100

    staampato in pagina lista di hotels

--- index.php
+++ index.php
@@ -37,4 +37,29 @@
             'distance_to_center' => 50
         ],
  ]
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+        <?php foreach($hotels as $hotel) :?>
+            <ul>
+                <?php foreach($hotel as $hotel_group) : ?>
+                <li>
+                    <?= $hotel_group?>
+                </li>
+                <?php endforeach?>
+            </ul>
+        <?php endforeach?>
+    
+</body>
+</html>
