8ae358440d9bc8f435c6910653ddca7855b3cfb1
LeonardoContino <leoconti2000@hotmail.it>
2023-02-07 15:35:22 +0100

    aggiunto tabella

--- index.php
+++ index.php
@@ -47,19 +47,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
     <title>Document</title>
 </head>
 <body>
     
-        <?php foreach($hotels as $hotel) :?>
-            <ul>
-                <?php foreach($hotel as $hotel_group) : ?>
-                <li>
-                    <?= $hotel_group?>
-                </li>
-                <?php endforeach?>
-            </ul>
-        <?php endforeach?>
+    
+
+<table class="table container mt-4">
+        <thead>
+            <tr>
+            <th scope="col">name</th>
+            <th scope="col">descriptions</th>
+            <th scope="col">parking</th>
+            <th scope="col">vote</th>
+            <th scope="col">distance</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($hotels as $hotel) :?>
+                <tr>
+                <?php foreach($hotel as $hotel_group) :?>
+                <td><?= $hotel_group ?></td>
+                
+                <?php endforeach ?>
+                </tr>
+            <?php endforeach ?>
+            
+        </tbody>
+</table>
     
 </body>
 </html>
